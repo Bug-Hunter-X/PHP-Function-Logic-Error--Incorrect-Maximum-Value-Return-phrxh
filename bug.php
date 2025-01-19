289364ee76498c1ca4640352693a1aed490e8361
BugHunter-X <askzenai@gmail.com>
2025-01-19 21:42:04 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,17 @@
+```php
+<?php
+function my_function($a, $b) {
+  if ($a > $b) {
+    return $a;
+  } else {
+    return $b; 
+  }
+}
+
+$result = my_function(5, 10);
+echo $result; // Outputs 10
+
+$result = my_function(10, 5);
+echo $result; // Outputs 10
+?>
+```
